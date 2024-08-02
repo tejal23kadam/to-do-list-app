0fa4ef2c63d3f2ac93e268d8b36943d6b12bdbe9
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-08-02 12:01:05 +0530

    editing done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { useState } from 'react';
+let nextId = 0;
 function App() {
+
+  const [inputToDo, setIputToDo] = useState('');
+  const [listToDos, setListToDos] = useState([]);
+  const [editing, setEditing] = useState(false);
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  const handleSubmit = (e) => {
+    setIputToDo(e.target.value);
+  };
+  const handleEditing = () => {
+    setEditing(true);
+  };
+  
+  const updateItem = (updatedTitle,id) => {
+    // 1. Find the todo with the provided id
+    const currentTodoIndex = listToDos.findIndex((todo) => todo.id === id);
+    // 2. Mark the todo as complete
+    const updatedTodo = { ...listToDos[currentTodoIndex], task: updatedTitle};
+    // 3. Update the todo list with the updated todo
+    const newTodos = [
+      ...listToDos.slice(0, currentTodoIndex),
+      updatedTodo,
+      ...listToDos.slice(currentTodoIndex + 1)
+    ];
+    setListToDos(newTodos);
+  }
+
+  const deleteItem = (id) => {
+    setListToDos((listToDos) => listToDos.filter((item) => item.id !== id));
+  };
+
+  const insertItem = () => {
+    const singleListItem = {
+      id: nextId++,
+      task: inputToDo
+    }
+    setListToDos(oldArray => [...oldArray, singleListItem]);
+    setIputToDo('')
+  }
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-color container">
+      <h1> to do list </h1>
+      <div className="inputFlex">
+        <div>
+          <input value={inputToDo} onChange={(e) => handleSubmit(e)} ></input>
+        </div>
+        <div>
+          <button onClick={insertItem} >Add</button>
+        </div>
+      </div>
+      <ul>
+        {listToDos.map(listToDo => (
+          <li key={listToDo.id}>
+            <div style={viewMode}>
+              <input value={listToDo.task}></input>
+              <button onClick={() => deleteItem(listToDo.id)}>delete</button>
+              <button onClick={handleEditing}>Edit</button>
+            </div>
+            
+              <input type='text' value={listToDo.task} className='editTextInput' style={editMode}
+                    onKeyDown={handleUpdatedDone}
+                    onChange={(e) => updateItem(e.target.value, listToDo.id)} />
+            
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
